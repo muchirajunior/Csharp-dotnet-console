eb67e70ea6e767daefd12e62779aa9dcce755f84
root <root@localhost.localdomain>
2021-07-22 16:30:46 +0300

    variables and user inputs

--- variables/Program.cs
+++ variables/Program.cs
@@ -4,11 +4,21 @@ namespace variables
 {
     class Program
     {
+        static void myvars(string n){
+            int x=10; //intenger variable
+            var y=20; //dynamic variable
+            double number=66.80; //floating variable 
+            string myname=" JUNIOR ";
+            Console.WriteLine($"number 1: {x} \n number 2:{y} \n decimal number :{number} ");
+            Console.WriteLine($"name 1: {n} \n name 2: {myname} ");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("enter your name :");
-            string name=Console.ReadLine(); 
-            Console.WriteLine("Name :"+name);
+            string name=Console.ReadLine();   
+            Console.WriteLine(" name :"+name);          
+            myvars(name);
         }
     }
 }
