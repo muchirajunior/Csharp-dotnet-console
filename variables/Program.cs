b83a80a4e9f8256a09c4ede6fe72f7a094f8691e
root <root@localhost.localdomain>
2021-07-22 15:36:03 +0300

    variables and user input in C#

---
+++ variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name :");
+            string name=Console.ReadLine(); 
+            Console.WriteLine("Name :"+name);
+        }
+    }
+}
