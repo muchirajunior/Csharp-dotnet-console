9b2401fe50fd21c9bdd41ec3951ad62356ee33b2
root <root@localhost.localdomain>
2021-07-23 16:25:43 +0300

    ascii characters and strings with interporation

---
+++ vars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		char b=(char)98;
+		Console.WriteLine(b);
+		int a ='a';
+		Console.WriteLine(a);
+		Console.WriteLine(b);
+		char[] letters={'j','u','n','i','o','r'};
+		string name=new string(letters);
+		Console.WriteLine(name);
+		//interpolation
+		string statement=$"my name is {name}";
+		Console.WriteLine(statement);
+	}
+}
