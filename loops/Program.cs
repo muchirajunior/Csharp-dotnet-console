819e77d8de20bc83e281023eb4d0c7e4ce52f835
root <root@localhost.localdomain>
2021-07-25 14:24:47 +0300

    loops in C#

---
+++ loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void while_loop(){
+            int a=0;
+            while (a<=10)
+            {
+              Console.WriteLine($"number is :{a}");
+              a++;  
+            }
+        }
+
+        static void for_loop(){
+            for (int i = 0; i <=10; i++)
+            {
+                Console.WriteLine($"The number is :{i}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            for_loop();
+        }
+    }
+}
