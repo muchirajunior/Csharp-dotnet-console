55107834e5fb9ade7336a8c896aa95bfeee51fbf
root <root@localhost.localdomain>
2021-07-25 14:23:41 +0300

    arrays in C#

---
+++ arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array=new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Enter number {i} :");
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n \narray is : ");
+            for (int i = 0; i < array.Length; i++)
+            {
+               Console.WriteLine(array[i]); 
+            }
+         }
+    }
+}
