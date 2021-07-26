5fbe1442587efeede0364b32af9d3e8acc610583
root <root@localhost.localdomain>
2021-07-26 21:59:37 +0300

    classes in C#

---
+++ classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace classes
+{
+    class Person{
+        public string name;
+
+        public void introduce(){
+            Console.WriteLine($"my name is {name}");
+        }
+
+        public static Person parse(string str){
+            var person=new Person();
+            person.name=str;
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person=Person.parse("John");
+            // person.name="MUCHIRA JUNIOR";
+            person.introduce();
+        }
+    }
+}
