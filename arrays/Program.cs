afaeccf908eb2d812c7f790b8c067f04bfd98118
root <root@localhost.localdomain>
2021-08-18 00:32:51 +0300

    linq selector changes

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace arrays
 {
@@ -17,6 +19,14 @@ namespace arrays
             {
                Console.WriteLine(array[i]); 
             }
+
+            List<int> numbers=new List<int>(){2,3,4,5,6,7,8,3,4};
+            var nums=from item in numbers where item<6 select item;
+            var n=numbers.Where((item)=>item<6);
+            var numbs=numbers.Select((item)=>item );
+            foreach (var item in numbs ) Console.WriteLine(item);
+
+           
          }
     }
 }
