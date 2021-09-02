ef77b155be6527f12351e25c6cf80fc6f36c5261
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2021-09-02 23:58:02 +0300

    new query project

---
+++ advancedqueries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advancedqueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
